1f7c71dd47b1a761656ddab17fbe2960c45cd48c
Hedzer Ferwerda <hedzer87@gmail.com>
2016-11-13 22:07:32 -0500

    Minor BindReceipt progress, and added to key

--- Framework/Classes/BindReceipt.js
+++ Framework/Classes/BindReceipt.js
@@ -4,13 +4,23 @@ import uid from 'Framework/Utilities/General/uid';
 import define from 'Framework/Utilities/Properties/addHiddenValue';
 import Enableable from 'Framework/Mixins/Enableable';
 
+//keys
+import to from 'Framework/Constants/Keys/BindReceipt/to';
+import on from 'Framework/Constants/Keys/BindReceipt/on';
+import oneWay from 'Framework/Constants/Keys/BindReceipt/oneWay';
+import twoWay from 'Framework/Constants/Keys/BindReceipt/twoWay';
+import normalize from 'Framework/Constants/Keys/BindReceipt/normalize';
+
 export default class BindReceipt extends Enableable(Receipt) {
 	constructor(relationship, subject) {
+		
 		define(this, $private, {
 			uid: uid(),
 			relationship: relationship,
 			subject: subject
 		});
+
+		this.to = this[to];
 	}
 	get uid() {
 		return this[$private].uid;
@@ -18,23 +28,48 @@ export default class BindReceipt extends Enableable(Receipt) {
 	set uid(id) {
 		this[$private].uid = id;
 	}
-	to(subject) {
+	[to](subject) {
 		let to = this[$private].to;
 		if (!to) {
 			this[$private].to = subject;
+			delete this.to;
+
+			//allow on, oneWay, twoWay, normalize
+			this.on = this[on];
+			this.oneWay = this[oneWay];
+			this.twoWay = this[twoWay];
+			this.normalize = this[normalize];
 		}
 		return this;
 	}
-	on(events) {
+	[on](events) {
+		delete this.on;
+		delete this.oneWay;
+		delete this.twoWay;
+
+		//do the things
+		
 		return this;
 	}
-	oneWay() {
+	[oneWay]() {
+		delete this.on;
+		delete this.oneWay;
+		delete this.twoWay;
+
+		//do the things
+		
 		return this;
 	}
-	twoWay() {
+	[twoWay]() {
+		delete this.on;
+		delete this.oneWay;
+		delete this.twoWay;
+
+		//do the things
+		
 		return this;
 	}
-	normalize(rules) {
+	[normalize](rules) {
 		return this;
 	}
 }
---
+++ Framework/Constants/Keys/BindReceipt/to.js
@@ -0,0 +1,4 @@
+import symbolOrString from 'Framework/Utilities/Properties/symbolOrString';
+
+let symbol = symbolOrString('BindReceipt.on');
+export default symbol;
