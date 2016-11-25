080b9e25a675f0d2e66ed3893172cd2710a6bded
Hedzer Ferwerda <hedzer87@gmail.com>
2016-11-25 17:13:17 -0500

    Added Receipt for Collection class where

---
+++ Framework/Classes/CollectionWhereReceipt.js
@@ -0,0 +1,20 @@
+import $private from 'Framework/Constants/Keys/General/private';
+import Receipt from 'Framework/Classes/Receipt';
+import define from 'Framework/Utilities/Properties/addHiddenValue';
+import Collection from 'Framework/Classes/Collection';
+
+export default class CollectionWhereReceipt extends Receipt {
+	constructor() {
+		super();
+		define(this, $private, {
+			selected: new Collection(),
+			rejected: new Collection()
+		});
+	}
+	get selected() {
+		return this[$private].selected;
+	}
+	get rejected() {
+		return this[$private].rejected;
+	}
+}
