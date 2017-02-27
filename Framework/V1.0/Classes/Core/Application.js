c5c9ebd31168f8f2cec757b8e80a339fb0306134
Hedzer Ferwerda <hedzer87@gmail.com>
2017-02-27 16:58:04 -0500

    Added Page loading logic to Application

--- Framework/V1.0/Classes/Core/Application.js
+++ Framework/V1.0/Classes/Core/Application.js
@@ -3,6 +3,9 @@ import Identity from '/Framework/V1.0/Classes/Core/Identity';
 import Distinct from '/Framework/V1.0/Classes/Core/Distinct';
 import Routable from '/Framework/V1.0/Mixins/Routable';
 import Router from '/Framework/V1.0/Singletons/Navigation/Router';
+import isElement from '/Framework/V1.0/TypeChecks/isElement';
+import isJSUI from '/Framework/V1.0/TypeChecks/isJSUI';
+import isPage from '/Framework/V1.0/TypeChecks/isPage';
 
 const identity = new Identity({
 	class: 'Application',
@@ -19,6 +22,39 @@ export default class Application extends Routable(Distinct) {
 	static get route() {
 		return 'Application';
 	}
+	static get Root() {
+		return document.body;
+	}
+	get Root() {
+		let root = (this.state('Root') || Application.Root);
+		return root;
+	}
+	set Root(HTMLElement) {
+		let element = false;
+		if (isElement(HTMLElement)) {
+			element = HTMLElement;
+		}
+		if (isJSUI(HTMLElement)) {
+			element = HTMLElement.element;
+		}
+		if (!element) { return; }
+		this.state('Root', element);
+	}
+	get Page() {
+		return this.state('Page');
+	}
+	set Page(page) {
+		if (!isPage(page)) { return; }
+		let previous = this.state('Page');
+		if (this.state('Page', page)) {
+			if (isPage(previous)) {
+				previous.remove();
+			}
+			page.addTo(this.Root);
+		}
+	}
+	get Navigation() {}
+	set Navigation(navigation) {}
 }
 
 //Application -> Roles -> Features -> Pages & Server
--- Framework/V1.0/Classes/Core/Page.js
+++ Framework/V1.0/Classes/Core/Page.js
@@ -1,6 +1,7 @@
 import Identity from '/Framework/V1.0/Classes/Core/Identity';
 import Main from '/Framework/V1.0/Classes/Elements/Main';
 import Routable from '/Framework/V1.0/Mixins/Routable';
+import isApplication from '/Framework/V1.0/TypeChecks/isApplication';
 
 const identity = new Identity({
 	class: 'Page',
@@ -11,6 +12,7 @@ export default class Page extends Routable(Main) {
 	constructor(){
 		super();
 		this.identity = identity;
+		this.Style.context = 'page';
 	}
 	static get route() {
 		return 'Page';
@@ -19,5 +21,17 @@ export default class Page extends Routable(Main) {
 		let instance = super.instantiate();
 		instance.addTo(document.body);
 		return instance;
+	}
+	set Context(context) {
+		if (context) {
+			let Application = context.Application;
+			if (isApplication(Application)) {
+				Application.Page = this;
+			}
+		}
+		super.Context = context;
+	}
+	get Context() {
+		return super.Context;
 	}
 }
