216345942d90c637e57d6306eee8d5c0837fa3d7
Hedzer Ferwerda <hedzer87@gmail.com>
2017-02-28 16:43:11 -0500

    Added built-in, overridable method auto-triggered by traversal completion

--- Framework/V1.0/Classes/Core/Endpoint.js
+++ Framework/V1.0/Classes/Core/Endpoint.js
@@ -2,7 +2,13 @@ import Routable from '/Framework/V1.0/Mixins/Routable';
 import JSUIFunction from '/Framework/V1.0/Classes/Core/Function';
 
 export default class Endpoint extends Routable(JSUIFunction) {
-	onRouteTraversed() {
-		this.execute();
+	constructor() {
+		super();
+		this.isRouteEndpoint = true;
+		this.original = this.function;
+	}
+	function(context) { /* endpoint code goes here */ }
+	onRouteCompleted(context) {
+		this.execute(context);
 	}
 }
