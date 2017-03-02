299b2b1e3a0288987e48e35a4d509e234a5140b0
Hedzer Ferwerda <hedzer87@gmail.com>
2017-03-02 15:29:23 -0500

    Added stateful mixin

--- Framework/V1.0/Classes/Core/Data.js
+++ Framework/V1.0/Classes/Core/Data.js
@@ -9,13 +9,14 @@ import Base from '/Framework/V1.0/Classes/Core/Base';
 //mixins
 import Extensible from '/Framework/V1.0/Mixins/Extensible';
 import Eventful from '/Framework/V1.0/Mixins/Eventful';
+import Stateful from '/Framework/V1.0/Mixins/Stateful';
 
 //typechecks
 import isString from '/Framework/V1.0/TypeChecks/isString';
 import isUndefined from '/Framework/V1.0/TypeChecks/isUndefined';
 import isObject from '/Framework/V1.0/TypeChecks/isObject';
 
-class Data extends Extensible(Eventful(Base)) {
+class Data extends Extensible(Eventful(Stateful(Base))) {
 	constructor(values) {
 		super();
 		this[$private].state = (isObject(values) ? values : {});
