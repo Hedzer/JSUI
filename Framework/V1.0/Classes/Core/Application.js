a2f71326a3d655f29882cd95da4cb77eaf7c20aa
Hedzer Ferwerda <hedzer87@gmail.com>
2017-03-06 12:04:00 -0500

    Added getter/setters & defaults for Router and Navigation

--- Framework/V1.0/Classes/Core/Application.js
+++ Framework/V1.0/Classes/Core/Application.js
@@ -6,6 +6,10 @@ import Router from '/Framework/V1.0/Singletons/Navigation/Router';
 import isElement from '/Framework/V1.0/TypeChecks/isElement';
 import isJSUI from '/Framework/V1.0/TypeChecks/isJSUI';
 import isPage from '/Framework/V1.0/TypeChecks/isPage';
+import isNavigation from '/Framework/V1.0/TypeChecks/isNavigation';
+import isRouter from '/Framework/V1.0/TypeChecks/isRouter';
+import isUPage from '/Framework/V1.0/TypeChecks/isUPage';
+import isUNavigation from '/Framework/V1.0/TypeChecks/isUNavigation';
 
 const identity = new Identity({
 	class: 'Application',
@@ -17,7 +21,12 @@ export default class Application extends Routable(Distinct) {
 		super('div');
 		this.identity = identity;
 		this[$private].routes = {};
-		Router.add(this);
+		let DefaultPage = this.constructor.Page;
+		let DefaultNavigation = this.constructor.Navigation;
+		let DefaultRouter = this.constructor.Router;
+		this.Page = (isUPage(DefaultPage) ? new DefaultPage() : false);
+		this.Navigation = (isUNavigation(DefaultNavigation) ? new DefaultNavigation() : false);
+		this.Router = (isRouter(DefaultRouter) ? DefaultRouter : Router);
 	}
 	static get Root() {
 		return document.body;
@@ -50,8 +59,39 @@ export default class Application extends Routable(Distinct) {
 			page.addTo(this.Root);
 		}
 	}
-	get Navigation() {}
-	set Navigation(navigation) {}
+	static get Page() {}
+	get Navigation() {
+		return this.state('Navigation');
+	}
+	set Navigation(navigation) {
+		if (!isNavigation(navigation)) { return; }
+		let previous = this.state('Navigation');
+		if (this.state('Navigation', navigation)) {
+			if (isNavigation(previous)) {
+				previous.remove();
+			}
+			navigation.addTo(this.Root);
+			navigation.map(this);
+		}
+	}
+	static get Navigation() {}
+	get Router() {
+		return this.state('Router');
+	}
+	set Router(router) {
+		console.log(router);
+		if (!isRouter(router)) { return; }
+		let previous = this.state('Router');
+		if (this.state('Router', router)) {
+			if (isRouter(previous)) {
+				previous.remove(this);
+			}
+			router.add(this);
+		}
+	}
+	static get Router() {
+		return Router;
+	}
 }
 
 //Application -> Roles -> Features -> Pages -> Endpoint
