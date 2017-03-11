b2296edb289472ac84fa313b438f16d175724546
Hedzer Ferwerda <hedzer87@gmail.com>
2017-03-11 15:57:10 -0500

    Changed paths to be less likely to collide with real strings

--- Framework/V1.0/TypeChecks/isPath.js
+++ Framework/V1.0/TypeChecks/isPath.js
@@ -5,7 +5,13 @@ import isString from '/Framework/V1.0/TypeChecks/isString';
 import exports from '/Framework/V1.0/Utilities/Dependencies/exports';
 
 export default function isPath(u) {
-	return (isString(u) && u.length > 0 && u.charAt(0) === '@');
+
+	if (!isString(u)) { return false; }
+
+	let count = u.length;
+	if (count < 4) { return false; }
+	
+	return (u.charAt(0) === '@' && u.charAt(1) === '{' && u.charAt(count - 1) === '}');
 }
 
 exports(isPath).as('/Framework/V1.0/TypeChecks/isPath');
