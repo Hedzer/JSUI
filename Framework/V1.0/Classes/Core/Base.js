c64321f4b1d99d0ca077247958c2e5df34e78f18
Hedzer Ferwerda <hedzer87@gmail.com>
2017-03-18 14:15:53 -0400

    Added expose functionality for potentially public members

--- Framework/V1.0/Classes/Core/Base.js
+++ Framework/V1.0/Classes/Core/Base.js
@@ -1,4 +1,7 @@
 
+//Constants
+import exposable from '/Framework/V1.0/Constants/Keys/General/exposable';
+
 //TypeChecks
 import isArray from '/Framework/V1.0/TypeChecks/isArray';
 import isFunction from '/Framework/V1.0/TypeChecks/isFunction';
@@ -42,6 +45,47 @@ export default class Base {
 		for (let i = 0; i < arguments.length; i++) {
 			let mixin = arguments[i];
 			result = mixin(result);
+		}
+
+		return result;
+	}
+	static exposes() {
+		if (!arguments.length) { /* throw error */ }
+		let result = this;
+
+		for (let i = 0; i < arguments.length; i++) {
+			let mixin = arguments[i];
+			let exposes = mixin.exposable;
+			if (!exposes) { continue; }
+			Object.keys(exposes).forEach((key) => {
+				let symbol = exposes[key];
+				if (symbol in this.prototype) {
+					let parent = Object.getPrototypeOf(this);
+					let descriptor = false;
+					while (!descriptor && parent) {
+						descriptor = Object.getOwnPropertyDescriptor(parent.prototype, symbol);
+						if (!descriptor) {
+							parent = Object.getPrototypeOf(parent);
+						}
+					}
+					if (descriptor) {
+						Object.defineProperty(parent.prototype, key, descriptor);
+					}
+				}
+				//add static descriptors
+				if (symbol in this) {
+					let parent = Object.getPrototypeOf(this);
+					let descriptor = false;
+					while (!descriptor && parent) {
+						descriptor = Object.getOwnPropertyDescriptor(parent, symbol);
+						if (!descriptor) {
+							parent = Object.getPrototypeOf(parent);
+						}
+						
+					}
+					Object.defineProperty(parent, key, descriptor);
+				}
+			});
 		}
 
 		return result;
