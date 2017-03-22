82edeb37d84898825a605b39f7ec93c17376cf4a
Hedzer Ferwerda <hedzer87@gmail.com>
2017-03-22 18:04:45 -0400

    Fixed bugs in eventful property adder

--- Framework/V1.0/Utilities/Properties/add.js
+++ Framework/V1.0/Utilities/Properties/add.js
@@ -4,18 +4,20 @@ import StateChangeReceipt from '/Framework/V1.0/Classes/Receipts/StateChange';
 
 //Constants
 import $private from '/Framework/V1.0/Constants/Keys/General/private';
+import $trigger from '/Framework/V1.0/Constants/Keys/General/trigger';
+import trigger from '/Framework/V1.0/Constants/Keys/Eventful/trigger';
 
 //Utilities
 import exports from '/Framework/V1.0/Utilities/Dependencies/exports';
 
-export default function add(host, name, defaultValue){
+export default function add(host, name, defaults){
 	Object.defineProperty(host, name, {
 		get:function(){
-			let value = (this[$private].state.hasOwnProperty(name) ? this[$private].state[name] : defaultValue);
+			let value = (name in this[$private].state ? this[$private].state[name] : defaults);
 			return value;
 		},
 		set:function(v){
-			let value = (this[$private].state.hasOwnProperty(name) ? this[$private].state[name] : defaultValue);
+			let value = (name in this[$private].state ? this[$private].state[name] : defaults);
 			let old = value;
 			value = v;
 			if (old !== v){
@@ -26,7 +28,7 @@ export default function add(host, name, defaultValue){
 					old: old,
 					new: value,
 				});
-				let trigger = (this.trigger || this.$trigger).bind(this);
+				let trigger = (this.trigger || this[$trigger] || this[trigger]).bind(this);
 				if (trigger){
 					trigger([`${name}Changed`, 'Changed'], data);
 				}
